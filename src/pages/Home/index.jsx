50783be91993452120ca130ebcf2a43f2c0bfb12
Adrian <adrianhenry2115@gmail.com>
2022-12-12 08:22:05 -0500

    home page layout update

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,23 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./index.scss";
-// import Cake from "../../assets/img/cakes/backdrop_cake.jpg";
+import Cake from "../../assets/img/cakes/main-cake.jpg";
 
 const Home = () => {
     return (
         <>
             <div className="home-container">
-                <div className="home-title-container">
-                    <div className="btn-wrapper">
-                        {/* <Link to="/contact">
-                            <button className="btn-container">
-                                <h4>Contact Us</h4>
-                            </button>
-                        </Link> */}
-                        {/* <button className="btn-container">
-                            <h4>Weddings</h4>
-                        </button> */}
-                    </div>
+                <div className="img-container">
+                    <img className="main-cake" src={Cake} alt="main-cake" />
                 </div>
             </div>
         </>
