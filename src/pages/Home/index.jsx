8a440737621fec0d75e3e6dcf7548deacc891b40
Adrian <adrianhenry2115@gmail.com>
2022-12-11 21:53:57 -0500

    home page pic and container change

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -8,8 +8,6 @@ const Home = () => {
         <>
             <div className="home-container">
                 <div className="home-title-container">
-                    <h3>Welcome To</h3>
-                    <h3 className="home-title">Molly's Specialty Sweets</h3>
                     <div className="btn-wrapper">
                         {/* <Link to="/contact">
                             <button className="btn-container">
